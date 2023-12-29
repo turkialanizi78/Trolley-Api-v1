6920634ddc16e1ecc7df928752b9d581e93546db
bofaissal alanizi <camycamat@gmail.com>
2023-12-29 10:50:41 +0300

    adding path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");  // Add this line
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
